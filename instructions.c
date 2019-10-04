5dbf8ae677cc39ff9a0e26a284efb27f9e98ed81
Eisuke Esaki <eesaki@student.42.us.org>
2019-10-03 18:14:31 -0700

    created instructions.c | empty funcs

---
+++ instructions.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   instructions.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eesaki <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/03 12:04:50 by eesaki            #+#    #+#             */
+/*   Updated: 2019/10/03 12:23:16 by eesaki           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	sa(int *list)
+{
+	UNUSED(list);
+}
+
+void	sb(int *list)
+{
+	UNUSED(list);
+}
+
+void	ss(int *list)
+{
+	UNUSED(list);
+}
+
+void	pa(int *list)
+{
+	UNUSED(list);
+}
+
+void	pb(int *list)
+{
+	UNUSED(list);
+}
+
+void	ra(int *list)
+{
+	UNUSED(list);
+}
+
+void	rb(int *list)
+{
+	UNUSED(list);
+}
+
+void	rr(int *list)
+{
+	UNUSED(list);
+}
+
+void	rra(int *list)
+{
+	UNUSED(list);
+}
+
+void	rrb(int *list)
+{
+	UNUSED(list);
+}
+
+void	rrr(int *list)
+{
+	UNUSED(list);
+}
