ad9a1dbb4f9d7035260232c0e304df1ce756e141
Eisuke Esaki <eesaki@student.42.us.org>
2019-10-10 22:46:47 -0700

    now creates lists from command line args and validates int range

--- checker.c
+++ checker.c
@@ -6,26 +6,103 @@
 /*   By: eesaki <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/06 20:48:25 by eesaki            #+#    #+#             */
-/*   Updated: 2019/10/06 21:50:45 by eesaki           ###   ########.fr       */
+/*   Updated: 2019/10/10 22:38:34 by eesaki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include "libft/libft.h"
+#include <stddef.h>
+#include <stdint.h>
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< debug
+#include <stdio.h>
+#include <limits.h>
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> debug
 
-void	verify_sort(t_stack *head_a, t_stack *head_b)
+t_stack	*newnode(int data);
+void	nodeappend(t_stack *head, t_stack *new);
+
+void	val_error(void)
 {
-	// check stack_a: ascending?
-	// check stack_b: empty?
+	write(1, "validation error", 17);
+	exit(1);
 }
 
-TODO		extract_a(int ac, char **av)
+void	print_stack(t_stack *head)
 {
-	// strsplit_2() all elms in av
+	while (head != NULL)
+	{
+		printf("n:%d\n", (int)head->n);
+		head = head->next;
+	}
+}
 
-	// alloc required nodes for each av elm and a head ptr
+// void	validate_sort(t_stack *head_a, t_stack *head_b)
+// {
+// 	// check stack_a: ascending?
+// 	// check stack_b: empty?
+// }
 
-	// atoi(split[i])->validate->assign ret val to each node
+// TODO		validate_dup(t_stack *head)
+// {
+	// TODO:	- traverse node->n and check for duplicates
+// }
 
+intmax_t	validate_int(intmax_t n)
+{
+	// printf("%jd\n", n);
+	intmax_t	dup;
+	if (!(INT_MIN <= n && n <= INT_MAX))
+		val_error();
+	// printf("%jd\n", n);
+
+	return (n);
+}
+
+t_stack	*build_a(char **av)
+{
+	size_t	i;
+	size_t	k;
+	char	**split;
+	t_stack	*head;
+	t_stack	*tmp;
+
+	i = 1;
+	k = 0;
+	head = NULL;
+	while (av[i])
+	{
+		split = ft_strsplit(av[i], ' ');
+		while (split[k])
+		{
+			tmp = newnode(validate_int(my_atoi(split[k])));
+			if (head == NULL)
+				head = tmp;
+			else
+				nodeappend(head, tmp);
+			k++;
+		}
+		k = 0;
+		i++;
+	}
+	return (head);
+}
+
+int		main(int ac, char **av)
+{
+	t_stack	*head_a;
+
+	head_a = NULL;
+	if (ac > 1)
+	{
+		head_a = build_a(av);
+	}
+	// validate duplicates
+	// validate_sort(head_a, head_b);
+
+	print_stack(head_a);
+
+	return (0);
 }
 
 // TODO:	- alloc stack_b
@@ -33,16 +110,4 @@ TODO		extract_a(int ac, char **av)
 //			- create array of pointers to instruction funs
 // TODO:	- dispatch all funcs in array of instructions. pass stack_a stack_b as args
 // TODO:	- implement instruction funcs
-// TODO:	- verify sorted result
-
-int		main(int ac, char **av)
-{
-	if (ac > 1)
-	{
-		extract_a(ac, av);
-	}
-
-	verify_sort(head_a, head b);
-
-	return (0);
-}
+// TODO:	- validate sorted result
--- push_swap.h
+++ push_swap.h
@@ -6,17 +6,33 @@
 /*   By: eesaki <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/03 12:21:01 by eesaki            #+#    #+#             */
-/*   Updated: 2019/10/06 21:10:06 by eesaki           ###   ########.fr       */
+/*   Updated: 2019/10/10 21:59:04 by eesaki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+# include <stdint.h>
+// # include "libft/libft.h"
 
 typedef struct		s_stack
 {
-	int				*n;
+	int				n;
 	struct s_stack	*next;
 }					t_stack;
+
+// typedef struct		s_list
+// {
+// 	int				*n;
+// 	struct s_list	*next;
+// }					t_list;
+
+/*
+**-----------------------------------------------------------------------------
+**									prototypes
+**-----------------------------------------------------------------------------
+*/
+void				error(void);
+intmax_t			my_atoi(const char *str);
 
 #endif
