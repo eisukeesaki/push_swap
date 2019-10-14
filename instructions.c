2639f20d62f5436482671f1fa52d3fe323c9a86c
Eisuke Esaki <eesaki@student.42.us.org>
2019-10-13 22:42:56 -0700

    added test purpose codes

--- instructions.c
+++ instructions.c
@@ -6,52 +6,75 @@
 /*   By: eesaki <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/03 12:04:50 by eesaki            #+#    #+#             */
-/*   Updated: 2019/10/06 21:12:46 by eesaki           ###   ########.fr       */
+/*   Updated: 2019/10/13 21:21:17 by eesaki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include <stdio.h> // test purpose
 
 void	sa(int *list __attribute((unused)))
 {
+	printf("sa called\n");
+	setbuf(stdout, NULL);
 }
 
 void	sb(int *list __attribute((unused)))
 {
+	printf("sb called\n");
+	setbuf(stdout, NULL);
 }
 
 void	ss(int *list __attribute((unused)))
 {
+	printf("ss called\n");
+	setbuf(stdout, NULL);
 }
 
 void	pa(int *list __attribute((unused)))
 {
+	printf("pa called\n");
+	setbuf(stdout, NULL);
 }
 
 void	pb(int *list __attribute((unused)))
 {
+	printf("pb called\n");
+	setbuf(stdout, NULL);
 }
 
 void	ra(int *list __attribute((unused)))
 {
+	printf("ra called\n");
+	setbuf(stdout, NULL);
 }
 
 void	rb(int *list __attribute((unused)))
 {
+	printf("rb called\n");
+	setbuf(stdout, NULL);
 }
 
 void	rr(int *list __attribute((unused)))
 {
+	printf("rr called\n");
+	setbuf(stdout, NULL);
 }
 
 void	rra(int *list __attribute((unused)))
 {
+	printf("rra called\n");
+	setbuf(stdout, NULL);
 }
 
 void	rrb(int *list __attribute((unused)))
 {
+	printf("rrb called\n");
+	setbuf(stdout, NULL);
 }
 
 void	rrr(int *list __attribute((unused)))
 {
+	printf("rrr called\n");
+	setbuf(stdout, NULL);
 }
