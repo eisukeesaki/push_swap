0b7ab8ff67ded504df6ea947c914a1b48d8c7ded
Eisuke Esaki <eesaki@student.42.us.org>
2019-11-07 01:21:09 -0800

    each instructions now calls print_stack() for debugging purposes

--- instructions.c
+++ instructions.c
@@ -6,75 +6,83 @@
 /*   By: eesaki <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/03 12:04:50 by eesaki            #+#    #+#             */
-/*   Updated: 2019/10/13 21:21:17 by eesaki           ###   ########.fr       */
+/*   Updated: 2019/10/17 22:55:49 by eesaki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h> // test purpose
 
-void	sa(int *list __attribute((unused)))
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< testing
+void	print_stack(t_stack *head);
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> testing
+
+// void	sa(t_stack *head __attribute((unused)))
+void	sa(t_stack *head)
 {
+	print_stack(head);
 	printf("sa called\n");
 	setbuf(stdout, NULL);
 }
 
-void	sb(int *list __attribute((unused)))
+// void	sb(t_stack *head __attribute((unused)))
+void	sb(t_stack *head)
 {
+	print_stack(head);
 	printf("sb called\n");
 	setbuf(stdout, NULL);
 }
 
-void	ss(int *list __attribute((unused)))
-{
-	printf("ss called\n");
-	setbuf(stdout, NULL);
-}
+// void	ss(t_stack *head __attribute((unused)))
+// {
+// 	printf("ss called\n");
+// 	setbuf(stdout, NULL);
+// }
 
-void	pa(int *list __attribute((unused)))
-{
-	printf("pa called\n");
-	setbuf(stdout, NULL);
-}
+// void	pa(t_stack *head __attribute((unused)))
+// {
+// 	printf("pa called\n");
+// 	setbuf(stdout, NULL);
+// }
 
-void	pb(int *list __attribute((unused)))
-{
-	printf("pb called\n");
-	setbuf(stdout, NULL);
-}
+// void	pb(t_stack *head __attribute((unused)))
+// {
+// 	printf("pb called\n");
+// 	setbuf(stdout, NULL);
+// }
 
-void	ra(int *list __attribute((unused)))
-{
-	printf("ra called\n");
-	setbuf(stdout, NULL);
-}
+// void	ra(t_stack *head __attribute((unused)))
+// {
+// 	printf("ra called\n");
+// 	setbuf(stdout, NULL);
+// }
 
-void	rb(int *list __attribute((unused)))
-{
-	printf("rb called\n");
-	setbuf(stdout, NULL);
-}
+// void	rb(t_stack *head __attribute((unused)))
+// {
+// 	printf("rb called\n");
+// 	setbuf(stdout, NULL);
+// }
 
-void	rr(int *list __attribute((unused)))
-{
-	printf("rr called\n");
-	setbuf(stdout, NULL);
-}
+// void	rr(t_stack *head __attribute((unused)))
+// {
+// 	printf("rr called\n");
+// 	setbuf(stdout, NULL);
+// }
 
-void	rra(int *list __attribute((unused)))
-{
-	printf("rra called\n");
-	setbuf(stdout, NULL);
-}
+// void	rra(t_stack *head __attribute((unused)))
+// {
+// 	printf("rra called\n");
+// 	setbuf(stdout, NULL);
+// }
 
-void	rrb(int *list __attribute((unused)))
-{
-	printf("rrb called\n");
-	setbuf(stdout, NULL);
-}
+// void	rrb(t_stack *head __attribute((unused)))
+// {
+// 	printf("rrb called\n");
+// 	setbuf(stdout, NULL);
+// }
 
-void	rrr(int *list __attribute((unused)))
-{
-	printf("rrr called\n");
-	setbuf(stdout, NULL);
-}
+// void	rrr(t_stack *head __attribute((unused)))
+// {
+// 	printf("rrr called\n");
+// 	setbuf(stdout, NULL);
+// }
