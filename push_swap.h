41bc4a661c710ce10522977544d5324d2841755e
Eisuke Esaki <eesaki@student.42.us.org>
2019-11-07 01:25:32 -0800

    created t_ins, added prototypes

--- push_swap.h
+++ push_swap.h
@@ -6,26 +6,33 @@
 /*   By: eesaki <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/03 12:21:01 by eesaki            #+#    #+#             */
-/*   Updated: 2019/10/10 21:59:04 by eesaki           ###   ########.fr       */
+/*   Updated: 2019/10/17 22:58:54 by eesaki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 # include <stdint.h>
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< testing
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> testing
 // # include "libft/libft.h"
 
+/*
+**-----------------------------------------------------------------------------
+**									structs
+**-----------------------------------------------------------------------------
+*/
 typedef struct		s_stack
 {
 	int				n;
 	struct s_stack	*next;
 }					t_stack;
 
-// typedef struct		s_list
-// {
-// 	int				*n;
-// 	struct s_list	*next;
-// }					t_list;
+typedef struct		s_ins
+{
+	char			*name;
+	void			(*ins)(t_stack *);
+}					t_ins;
 
 /*
 **-----------------------------------------------------------------------------
@@ -34,5 +41,25 @@ typedef struct		s_stack
 */
 void				error(void);
 intmax_t			my_atoi(const char *str);
+int					get_next_line(const int fd, char **line);
+
+t_stack				*newnode(int data);
+void				ins_node_aft(t_stack *head, t_stack *new);
+
+void				sa(t_stack *head);
+void				sb(t_stack *head);
+
+// t_ins				ins_table[2] =
+// {
+// 	{"sa", sa},
+// 	{"sb", sb},
+// };
+
+// typedef struct		s_list
+// {
+// 	int				*n;
+// 	struct s_list	*next;
+// }					t_list;
+
 
 #endif
