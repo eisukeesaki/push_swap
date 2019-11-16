f301a26b72501e3ec24e5cff8150de4b416ec571
Eisuke Esaki <eesaki@student.42.us.org>
2019-11-15 18:05:13 -0800

    reading/validation of instruction is now working

--- validate_instructions.c
+++ validate_instructions.c
@@ -6,46 +6,47 @@
 /*   By: eesaki <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/14 20:34:15 by eesaki            #+#    #+#             */
-/*   Updated: 2019/11/15 00:00:53 by eesaki           ###   ########.fr       */
+/*   Updated: 2019/11/15 17:58:11 by eesaki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "libft/libft.h"
 #include <stddef.h>
+#define NVALID 11
 
 void	compare(char *ins, char *valid[])
 {
 	size_t	i;
 
 	i = 0;
-	while (valid[i])
+	while (i < NVALID)
 	{
-		if (!(ft_strequ(ins, valid[i])))
-		{
-			write(1, "invalid ins", 12);
-			exit(1);
-		}
+		if (ft_strequ(ins, valid[i]))
+			return ;
 		i++;
 	}
+	// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< replace with error()
+	write(1, "invalid ins", 12);
+	exit(1);
+	// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> replace with error()
 }
 
 void	vali_ins(char *ins)
 {
-	char	*valid[] =
+	char	*valid[NVALID] =
 	{
-		"sa\n",
-		"sb\n",
-		"ss\n",
-		"pa\n",
-		"pb\n",
-		"ra\n",
-		"rb\n",
-		"rr\n",
-		"rra\n",
-		"rrb\n",
-		"rrr\n",
-		"\0",
+		"sa",
+		"sb",
+		"ss",
+		"pa",
+		"pb",
+		"ra",
+		"rb",
+		"rr",
+		"rra",
+		"rrb",
+		"rrr",
 	};
 	compare(ins, valid);
 }
