230f86d148843f5cb4d9a4f8f2113fd749ec05ea
Eisuke Esaki <eesaki@student.42.us.org>
2020-02-16 20:31:56 -0800

    moved error.c to src/. error() now accepts error messages as arg

--- src/error.c
+++ src/error.c
@@ -6,15 +6,20 @@
 /*   By: eesaki <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 16:28:13 by eesaki            #+#    #+#             */
-/*   Updated: 2019/10/08 16:32:11 by eesaki           ###   ########.fr       */
+/*   Updated: 2020/02/16 20:21:56 by eesaki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include <unistd.h>
 #include <stdlib.h>
 
-void	error(void)
+void	error(char *message)
 {
+	t_bool		debug;
+
 	write(1, "Error\n", 7);
+	if ((debug = 1))/* disable it when dev is complete! */
+		ft_putstr(message);
 	exit(1);
 }
